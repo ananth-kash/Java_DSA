b39eb227ef60bf2801f5859ee0ec5494b45ed465
Ananth Kashyap <you@example.com>
2025-04-02 16:08:59 +0530

    first commit

---
+++ 2 Array/Easy/rem_duplicates.java
@@ -0,0 +1,23 @@
+
+public class rem_duplicates {
+    int removeDuplicates(int[] arr){
+        int n = arr.length;
+        int i=0;
+        int j=1;
+        while(j<n){
+            if(arr[i]!=arr[j]){
+            i++;
+            arr[i]=arr[j];
+            }   
+            j++;
+        }
+        
+        return i+1;
+    }
+
+    public static void main(String [] args){
+        int a[]={2,10,20,20,20,40,50,50};
+        rem_duplicates r1= new rem_duplicates();
+        System.out.println(r1.removeDuplicates(a));
+    }    
+}
